7bee4f2edcacf31384278c487ac442c4b494ccbe
Marcelo Guerra <mbg1998137@gmail.com>
2023-01-03 19:24:04 -0300

    configuracoes do DataContext

--- 5_Fundamentos_Do_Entity_Framework/Blog/Data/BlogDataContext.cs
+++ 5_Fundamentos_Do_Entity_Framework/Blog/Data/BlogDataContext.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Blog.Data
 {
-    public class BlogDataContext
+    public class BlogDataContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Blog;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+
     }
 }
