9401a1f5c05fa2128581b0705860f555589f63cb
Marcelo Guerra <mbg1998137@gmail.com>
2023-01-04 20:19:15 -0300

    Aula de Log

--- 5_Fundamentos_Do_Entity_Framework/Blog/Data/BlogDataContext.cs
+++ 5_Fundamentos_Do_Entity_Framework/Blog/Data/BlogDataContext.cs
@@ -11,10 +11,12 @@ namespace Blog.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<User> Users { get; set; }
-        
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Blog;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Blog;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            options.LogTo(Console.WriteLine);
+        }
 
     }
 }
