b37ddb8c992d53d939755ed5ec0c7bfd63072880
Marcelo Guerra <mbg1998137@gmail.com>
2023-01-04 20:11:51 -0300

    Include

--- 5_Fundamentos_Do_Entity_Framework/Blog/Program.cs
+++ 5_Fundamentos_Do_Entity_Framework/Blog/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Blog.Data;
 using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 
@@ -10,37 +11,15 @@ namespace Blog
         static void Main(string[] args)
         {
             using var context = new BlogDataContext();
-
-            var user = new User
-            {
-                Name = "Marcelo Guerra",
-                Slug = "marceloguerra",
-                Email = "marcelo@balta.io",
-                Bio = "Estudante",
-                Image = "https://balta.io",
-                PasswordHash = "24012998"
-            };
-
-            var category = new Category
-            {
-                Name = "Backend",
-                Slug = "backend"
-            };
-
-            var post = new Post
-            {
-                Author = user,
-                Category = category,
-                Body = "<p>Hello World</p>",
-                Slug = "comecando-com-ef-core",
-                Summary = "Neste artigo estou aprendendo EF Core",
-                Title = "Começando com EF Core",
-                CreateDate = DateTime.Now,
-                LastUpdateDate = DateTime.Now
-            };
-
-            context.Posts.Add(post);
-            context.SaveChanges();
-        }
+            var posts = context
+                .Posts
+                .AsNoTracking()   
+                .Include(x => x.Author)
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToList();
+            
+            foreach (var post in posts)
+                Console.WriteLine($"{post.Title} escrito por {post.Author?.Name}");
+        }   
     }
 }
