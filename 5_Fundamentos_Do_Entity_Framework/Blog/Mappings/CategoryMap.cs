7fb45e4a4735f18f81a4bd02df81b96dc765fa8f
Marcelo Guerra <mbg1998137@gmail.com>
2023-01-06 23:29:42 -0300

    Mapping

--- 5_Fundamentos_Do_Entity_Framework/Blog/Mappings/CategoryMap.cs
+++ 5_Fundamentos_Do_Entity_Framework/Blog/Mappings/CategoryMap.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Blog.Mappings
 {
-    internal class CategoryMap
+    public class CategoryMap : IEntityTypeConfiguration<Category>
     {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            //Tabela
+            builder.ToTable("Category");
+            
+            //Primary Key
+            builder.HasKey(x => x.Id);
+
+            //Identity
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn(); //IDENTITY (1,1)
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")                
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Slug)
+               .IsRequired()
+               .HasColumnName("Slug")
+               .HasColumnType("VARCHAR")
+               .HasMaxLength(80);
+
+            //Índice
+            builder.HasIndex(x => x.Slug, "IX_Category_Slug")
+                .IsUnique();
+
+        }
     }
 }
