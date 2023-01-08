fb3ab732788a9335b00fa419b88086bd03622cd2
Marcelo Guerra <mbg1998137@gmail.com>
2023-01-08 18:04:04 -0300

    UserMap

---
+++ 5_Fundamentos_Do_Entity_Framework/Blog/Mappings/UserMap.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+            
+            builder.HasKey(x => x.Id);
+            
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Bio);
+            builder.Property(x => x.Email);
+            builder.Property(x => x.Image);
+            builder.Property(x => x.PasswordHash);
+
+            builder.Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
+            
+            builder
+                .HasIndex(x =>x.Slug, "IX_User_Slug")
+                .IsUnique();
+        }
+    }
+}
