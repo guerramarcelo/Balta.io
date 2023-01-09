6f7fe4d74b3dd93cc086cc242eae9655a55acf4b
Marcelo Guerra <mbg1998137@gmail.com>
2023-01-09 18:51:28 -0300

    Adicionando campo GitHub

--- 5_Fundamentos_Do_Entity_Framework/Blog/Mappings/UserMap.cs
+++ 5_Fundamentos_Do_Entity_Framework/Blog/Mappings/UserMap.cs
@@ -30,6 +30,7 @@ namespace Blog.Mappings
             builder.Property(x => x.Email);
             builder.Property(x => x.Image);
             builder.Property(x => x.PasswordHash);
+            builder.Property(x => x.GitHub);
 
             builder.Property(x => x.Slug)
                 .IsRequired()
--- 5_Fundamentos_Do_Entity_Framework/Blog/Models/User.cs
+++ 5_Fundamentos_Do_Entity_Framework/Blog/Models/User.cs
@@ -8,6 +8,7 @@ namespace Blog.Models
         public string Name { get; set; }
         public string Email { get; set; }      
         public string PasswordHash { get; set; }
+        public string GitHub { get; set; }
         public string Bio { get; set; }
         public string Image { get; set; }
         public string Slug { get; set; }
