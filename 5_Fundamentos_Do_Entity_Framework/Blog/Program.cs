45f5fd567a1773a64d9e8135508910457ecdbdc5
Marcelo Guerra <mbg1998137@gmail.com>
2023-01-10 19:21:33 -0300

    Finalizando

--- 5_Fundamentos_Do_Entity_Framework/Blog/Program.cs
+++ 5_Fundamentos_Do_Entity_Framework/Blog/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Blog
+﻿using Blog.Data;
+using System;
+
+namespace Blog
 {
     public class Program
     {
         static void Main(string[] args)
         {
-       
+            using (var context = new BlogDataContext())
+            {
+                var user = context.Users;
+                foreach (var item in user)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            
+            
         }   
     }
 }
