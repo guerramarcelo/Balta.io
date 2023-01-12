a034d4f07c9dcf1f3e4d3c2a9ba3db2d503a52d3
Marcelo Guerra <mbg1998137@gmail.com>
2023-01-12 17:02:36 -0300

    CRUD

--- 6 - Fundamentos do ASP.NET 6/Todo/Controllers/HomeController.cs
+++ 6 - Fundamentos do ASP.NET 6/Todo/Controllers/HomeController.cs
@@ -4,13 +4,65 @@ using Todo.Models;
 
 namespace Todo.Controllers
 {
-    [ApiController]   
+    [ApiController]
     public class HomeController : ControllerBase
     {
-        [HttpGet ("/")]        
-       public List<TodoModel> Get([FromServices] AppDbContext context)
+        [HttpGet("/")]
+        public List<TodoModel> Get([FromServices] AppDbContext context)
         {
             return context.Todos.ToList();
         }
+
+        [HttpGet("/{id:int}")]
+        public TodoModel GetById(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            return context.Todos.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost("/")]
+        public TodoModel Post(
+            [FromBody] TodoModel todo,
+            [FromServices] AppDbContext context
+            )
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();
+            return todo;
+
+        }
+
+        [HttpPut("/{id:int}")]
+        public TodoModel Put(
+          [FromRoute] int id,
+          [FromBody] TodoModel todo,
+          [FromServices] AppDbContext context
+          )
+        {
+            var todos = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (todos == null)
+                return todo;
+            
+            todos.Title = todo.Title;
+            todos.Done= todo.Done;
+
+            context.Todos.Update(todos);
+            context.SaveChanges();
+            return todos;
+        }
+
+        [HttpDelete("/{id:int}")]
+        public TodoModel Delete(
+         [FromRoute] int id,         
+         [FromServices] AppDbContext context
+         )
+        {
+            var todos = context.Todos.FirstOrDefault(x => x.Id == id);            
+            context.Todos.Remove(todos);
+            context.SaveChanges();
+            return todos;
+        }
+
     }
 }
