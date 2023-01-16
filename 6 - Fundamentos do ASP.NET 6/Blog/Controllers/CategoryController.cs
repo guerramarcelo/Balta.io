d1e57274a2b602df823f7c1b2229613640c30426
Marcelo Guerra <mbg1998137@gmail.com>
2023-01-16 17:32:42 -0300

    Versionamento

---
+++ 6 - Fundamentos do ASP.NET 6/Blog/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("v1/categories")] 
+        public IActionResult Get([FromBody] BlogDataContext context)
+        {
+            var categories = context.Categories.ToList();
+            return Ok(categories);
+        }
+
+
+    }
+}
