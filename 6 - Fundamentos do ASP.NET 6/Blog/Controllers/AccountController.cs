ec595683316f708d78b93d397ba50877e4a81d6f
Marcelo Guerra <mbg1998137@gmail.com>
2023-01-22 20:41:20 -0300

    CRUD User

--- 6 - Fundamentos do ASP.NET 6/Blog/Controllers/AccountController.cs
+++ 6 - Fundamentos do ASP.NET 6/Blog/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
-﻿using Blog.Services;
+﻿using Blog.Data;
+using Blog.Extensions;
+using Blog.Models;
+using Blog.Services;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
@@ -6,17 +10,34 @@ namespace Blog.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        private readonly TokenService _tokenService;
-        public AccountController(TokenService tokenService)
-        { 
-            _tokenService = tokenService;
+        [HttpPost ("v1/accounts/")]
+        public async Task<IActionResult> Post(
+            [FromBody] RegisterViewModel model,
+            [FromServices] BlogDataContext context
+            )
+        {
+            if(!ModelState.IsValid) 
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+            var user = new User 
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Slug = model.Email.Replace(oldValue:"@", newValue:"-")
+                .Replace(oldValue:".", newValue:"-")
+            };
+
         }
-        
-        [HttpPost ("v1/login")]
-        public IActionResult Login()
-        {            
-            var token = _tokenService.GenerateToken(null);
+
+        [HttpPost("v1/accounts/login")]
+        public IActionResult Login([FromServices]TokenService tokenService)
+        {
+            var token = tokenService.GenerateToken(null);
             return Ok(token);
         }
+
+        
+
+        
     }
 }
