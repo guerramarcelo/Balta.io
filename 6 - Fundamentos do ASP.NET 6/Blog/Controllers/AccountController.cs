df9ec0c008fd76a3c62793c8343f704890ce19b2
Marcelo Guerra <mbg1998137@gmail.com>
2023-01-23 17:56:57 -0300

    Password Generator e LoginViewModel

--- 6 - Fundamentos do ASP.NET 6/Blog/Controllers/AccountController.cs
+++ 6 - Fundamentos do ASP.NET 6/Blog/Controllers/AccountController.cs
@@ -4,6 +4,8 @@ using Blog.Models;
 using Blog.Services;
 using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
 
 namespace Blog.Controllers
 {
@@ -27,7 +29,41 @@ namespace Blog.Controllers
                 .Replace(oldValue:".", newValue:"-")
             };
 
+            var password = PasswordGenerator.Generate(
+                length: 25,
+                includeSpecialChars: true,
+                upperCase: false); //para gerar a senha inicial, que pode ser alterada
+
+            user.PasswordHash = PasswordHasher.Hash(password);
+
+            try
+            {
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    user = user.Email, password
+                }));
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(
+                    400,
+                    value: new ResultViewModel<string>
+                    ("05x099 - Este E-mail já está cadastrado!"));
+            }
+            catch
+            {
+                return StatusCode(
+                    500,
+                    value: new ResultViewModel<string>
+                    ("05X04 - Falha interna no servidor"));
+            }
+            
+
         }
+
 
         [HttpPost("v1/accounts/login")]
         public IActionResult Login([FromServices]TokenService tokenService)
