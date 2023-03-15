59015b48b816df8f5eb66296acfe6c4b9512ba33
guerramarcelo <mbg1998137@gmail.com>
2023-03-15 10:18:13 -0300

    modelando o domínio

---
+++ 9 - Refatorando para testes de unidade/store/Domain/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Store.Domain.Entities
+{
+    public class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+    
+}
