55685061da9559ef45bf3acab92a31f587f881a3
guerramarcelo <mbg1998137@gmail.com>
2023-03-16 09:10:43 -0300

    Testando queries

---
+++ 9 - Refatorando para testes de unidade/Store.Tests/Repositories/FakeDeliveryFeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Store.Domain.Repositories.Interfaces;
+
+namespace Store.Tests.Repositories
+{
+    public class FakeDeliveryFeeRepository : IDeliveryFeeRepository
+    {
+        public decimal Get(string zipCode)
+        {
+            return 10;
+        }
+    }
+}
